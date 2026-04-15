9250208d4213ee46182c57a33d4bf95133a4036b
krishna <krishna40tiwari@gmail.com>
2026-04-15 13:09:36 +0530

    updated API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://blog-content-management-system-full-stack.onrender.com/api/",
   headers: { "Content-Type": "application/json" },
 });
 
